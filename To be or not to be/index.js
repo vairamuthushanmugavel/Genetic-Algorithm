556a0dc94ecaa913f987e96ee973db0250cb7eac
vairamuthu shanmugavel <vairamuthu.sv@zohocorp.com>
2020-04-12 20:52:20 +0530

    updating the ui

---
+++ To be or not to be/index.js
@@ -0,0 +1,16 @@
+var textNode = document.getElementById("text");
+var generationNode = document.getElementById("generation");
+var populationNode = document.getElementById("population");
+var avgfitnessNode = document.getElementById("averagefitness");
+var mutratioNode = document.getElementById("mutratio");
+//text to be guesses.
+var text = "to be or not to be";
+var mutationRation = 0.1;
+var population = 200;
+population;
+function displayInfo() {
+  textNode.textContent = text;
+  mutratioNode.textContent = mutationRation * 100 + "%";
+  populationNode.textContent = population;
+}
+displayInfo();
